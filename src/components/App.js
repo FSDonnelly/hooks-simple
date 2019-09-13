eb68465e8c400fccfc97ef9e338c893bb3feed00
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-12 21:06:10 -0700

    refacot App.js to func and useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
-  return <div className='App'>My App</div>;
-}
+const App = () => {
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div className='ui container'>
+      <div>
+        <button
+          className='ui inverted purple button'
+          onClick={() => setResource('posts')}
+        >
+          Posts
+        </button>
+        <button
+          className='ui inverted blue button'
+          onClick={() => setResource('todos')}
+        >
+          Todos
+        </button>
+      </div>
+      {resource}
+    </div>
+  );
+};
 
 export default App;
